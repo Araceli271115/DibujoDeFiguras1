2fefa663c2168c0d4910f5d904d5d6f2d1b277d8
JorgeJasso <JassoJorgeAlberto@gmail.com>
2017-02-23 19:38:20 -0600

    Add files via upload
    Aquí está el proyecto de figuras que pidió el maestro y puse como extra que se pueda agregar texto en el panel, además  modifique el mouseDragged y paintComponent para que mostrara las figuras al estarce creando

---
+++ proyecto3/src/proyecto3/MiOvalo.java
@@ -0,0 +1,26 @@
+package proyecto3;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class MiOvalo extends MiFiguraDelimitada {
+
+    public MiOvalo() {
+        super();
+    }
+
+    public MiOvalo(int x1, int y1, int x2, int y2, Color color, boolean relleno) {
+        super(x1, y1, x2, y2, color, relleno);
+
+    }
+
+    @Override
+    public void dibujar(Graphics g) {
+        g.setColor(getObtenerColor());
+        if (getObtenerRelleno() == true) {
+            g.fillOval(calcularX1(), calcularY1(), anchura(), altura());
+        } else {
+            g.drawOval(calcularX1(), calcularY1(), anchura(), altura());
+        }
+    }
+}
